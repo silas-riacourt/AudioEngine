04c8161a134a138468201789018d08f249517775
Silass_ <silasdu22@gmail.com>
2019-11-28 15:26:12 +0100

    v0.1 fix : library importé

--- src/fr/audioengine/controler/Audio.java
+++ src/fr/audioengine/controler/Audio.java
@@ -5,11 +5,8 @@ public class Audio {
 
 
 	public static void makeSound() {
-		int numbeeps = 10;
-		System.out.println("hdjd");
-		for(int x=0;x<numbeeps;x++)
-		{
-		  java.awt.Toolkit.getDefaultToolkit().beep();
-		}
+
+		System.out.println("ok");
+
 	}
 }
