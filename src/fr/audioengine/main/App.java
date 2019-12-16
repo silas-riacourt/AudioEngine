f4226e81f0b8028c549aeb0d620382a7595f6e1c
Silass_ <silasdu22@gmail.com>
2019-12-16 14:59:08 +0100

    v0.4 feat : update clip volume

--- src/fr/audioengine/main/App.java
+++ src/fr/audioengine/main/App.java
@@ -1,34 +1,45 @@
 package fr.audioengine.main;
 
-import java.util.concurrent.TimeUnit;
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 
 import fr.audioengine.controler.MediaPlayerHome;
 import fr.audioengine.model.Player;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class App extends Application{
+public class App extends Application {
 
-	
-	//static private Home home = new Home();
+	// static private Home home = new Home();
 
-	
 	static private MediaPlayerHome mediaPlayer = new MediaPlayerHome();
-	
+
 	public static void main(String[] args) {
 		launch(args);
-	
+
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
 		//mediaPlayer.getStage().show();
-		Player p = new Player("trap.wav",true);
-		p.play();
 
-		//Player p1 = new Player("Loop.wav");
-		//p1.play();
+		Player p = new Player("ambiance_4.wav",true);
+		p.setVolume(-10.0f);
+
+		Thread.sleep(2000);
+		Player p2 = new Player("coin.wav",2);
+		p2.setVolume(0.0F);
+
+		Thread.sleep(3000);
+		p2.restart();
+		
+		Player p3 = new Player("heal.wav",1);	
+		
 	}
 
 }
--- src/fr/audioengine/model/Player.java
+++ src/fr/audioengine/model/Player.java
@@ -6,8 +6,12 @@ import java.io.IOException;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.Port;
 import javax.sound.sampled.UnsupportedAudioFileException;
+
+import javafx.scene.media.MediaPlayer;
 
 public class Player {
 	
@@ -18,43 +22,65 @@ public class Player {
 	String path;
 
 	AudioInputStream audioInputStream;
+	
+	
 
 	public Player(String path,Boolean loop) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
 
 		this.path = path;
-		clip = AudioSystem.getClip();
-		audioInputStream = AudioSystem.getAudioInputStream(new File("src/fr/audioengine/samples/"+this.path).getAbsoluteFile());
-		clip.open(audioInputStream);
+		//this.audioInputStream = AudioSystem.getAudioInputStream(new File("src/fr/audioengine/samples/"+path));
+		this.audioInputStream = AudioSystem.getAudioInputStream(new File("src/fr/audioengine/samples/"+path).getAbsoluteFile());
+		this.clip = AudioSystem.getClip();
+		this.clip.open(audioInputStream);
 		if(loop) {
 			clip.loop(Clip.LOOP_CONTINUOUSLY);
+		}else {
+			clip.start();
 		}
 
-	}
 
+	}
+	public Player(String path,int loop) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+
+		this.path = path;
+		this.audioInputStream = AudioSystem.getAudioInputStream(new File("src/fr/audioengine/samples/"+path));
+		this.clip = AudioSystem.getClip();
+		this.clip.open(audioInputStream);
+		this.clip.loop(loop-1);
+
+
+
+	}
 	public void play() {
-			clip.start();
+
+			this.clip.start();
 
 
 	}
+	public void setVolume(float f) {
 
+	            FloatControl volumeControl = (FloatControl) this.clip.getControl(FloatControl.Type.MASTER_GAIN);
+	            volumeControl.setValue(f);
 
+	}
+	 
 	public boolean pause() {
-			clip.stop();
+			this.clip.stop();
 			return true;
 		}
 
 
 	public void restart() throws IOException, LineUnavailableException, UnsupportedAudioFileException {
 		this.stop();
-		clip = AudioSystem.getClip();
-		audioInputStream = AudioSystem.getAudioInputStream(new File("src/fr/audioengine/samples/"+this.path).getAbsoluteFile());
-		clip.open(audioInputStream);
+		this.clip = AudioSystem.getClip();
+		this.audioInputStream = AudioSystem.getAudioInputStream(new File("src/fr/audioengine/samples/"+path));
+		this.clip.open(audioInputStream);
 		this.play();
 	}
 
 	public void stop() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-		clip.stop();
-		clip.close();
+		this.clip.stop();
+		this.clip.close();
 	}
 
 
