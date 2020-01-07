1d972c6ad6e2a253a8f8cbaea036c1b1b415df63
Silass_ <silasdu22@gmail.com>
2020-01-07 19:46:03 +0100

    v0.5 feat : loop clip

--- src/fr/audioengine/main/App.java
+++ src/fr/audioengine/main/App.java
@@ -7,7 +7,6 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.FloatControl;
 
-import fr.audioengine.controler.MediaPlayerHome;
 import fr.audioengine.model.Player;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -15,8 +14,6 @@ import javafx.stage.Stage;
 public class App extends Application {
 
 	// static private Home home = new Home();
-
-	static private MediaPlayerHome mediaPlayer = new MediaPlayerHome();
 
 	public static void main(String[] args) {
 		launch(args);
@@ -29,16 +26,20 @@ public class App extends Application {
 		//mediaPlayer.getStage().show();
 
 		Player p = new Player("ambiance_4.wav",true);
-		p.setVolume(-10.0f);
+		p.setVolume(0);
+		p.play();
+		p.setVolume(0.7f);
 
 		Thread.sleep(2000);
-		Player p2 = new Player("coin.wav",2);
-		p2.setVolume(0.0F);
+		Player p2 = new Player("coin.wav",5);
+		p2.play();
+		p2.setVolume(0.9F);
 
 		Thread.sleep(3000);
 		p2.restart();
 		
-		Player p3 = new Player("heal.wav",1);	
+		Player p3 = new Player("heal.wav",1);
+		p3.play();
 		
 	}
 
--- src/fr/audioengine/model/Player.java
+++ src/fr/audioengine/model/Player.java
@@ -14,66 +14,77 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 import javafx.scene.media.MediaPlayer;
 
 public class Player {
-	
+
 	Long currentFrame;
-	
+
 	Clip clip;
-	
+
 	String path;
 
 	AudioInputStream audioInputStream;
-	
-	
 
-	public Player(String path,Boolean loop) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+	public Player(String path, Boolean loop)
+			throws UnsupportedAudioFileException, IOException, LineUnavailableException {
 
 		this.path = path;
-		//this.audioInputStream = AudioSystem.getAudioInputStream(new File("src/fr/audioengine/samples/"+path));
-		this.audioInputStream = AudioSystem.getAudioInputStream(new File("src/fr/audioengine/samples/"+path).getAbsoluteFile());
+		// this.audioInputStream = AudioSystem.getAudioInputStream(new
+		// File("src/fr/audioengine/samples/"+path));
+		this.audioInputStream = AudioSystem
+				.getAudioInputStream(new File("src/fr/audioengine/samples/" + path).getAbsoluteFile());
 		this.clip = AudioSystem.getClip();
 		this.clip.open(audioInputStream);
-		if(loop) {
+		if (loop) {
 			clip.loop(Clip.LOOP_CONTINUOUSLY);
-		}else {
-			clip.start();
+			clip.stop();
 		}
 
-
 	}
-	public Player(String path,int loop) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+
+	public Player(String path, int loop) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
 
 		this.path = path;
-		this.audioInputStream = AudioSystem.getAudioInputStream(new File("src/fr/audioengine/samples/"+path));
+		this.audioInputStream = AudioSystem.getAudioInputStream(new File("src/fr/audioengine/samples/" + path));
 		this.clip = AudioSystem.getClip();
 		this.clip.open(audioInputStream);
-		this.clip.loop(loop-1);
-
-
+		this.clip.loop(loop - 1);
 
 	}
+
 	public void play() {
 
-			this.clip.start();
-
-
-	}
-	public void setVolume(float f) {
-
-	            FloatControl volumeControl = (FloatControl) this.clip.getControl(FloatControl.Type.MASTER_GAIN);
-	            volumeControl.setValue(f);
+		this.clip.start();
 
 	}
-	 
-	public boolean pause() {
-			this.clip.stop();
-			return true;
+
+	public boolean setVolume(float f) {
+		
+		boolean status = true;
+
+		if (!clip.isOpen()) {
+			status = false;
+		} else {
+
+			if (f < 0f || f > 1f) throw new IllegalArgumentException("Volume not valid: " + f);
+			
+			
+			FloatControl volumeControl = (FloatControl) this.clip.getControl(FloatControl.Type.MASTER_GAIN);
+			float range = volumeControl.getMaximum() - volumeControl.getMinimum();
+			float gain = (range * f) + volumeControl.getMinimum();
+			volumeControl.setValue(gain);
+
 		}
+		return status;
+	}
 
+	public boolean pause() {
+		this.clip.stop();
+		return true;
+	}
 
 	public void restart() throws IOException, LineUnavailableException, UnsupportedAudioFileException {
 		this.stop();
 		this.clip = AudioSystem.getClip();
-		this.audioInputStream = AudioSystem.getAudioInputStream(new File("src/fr/audioengine/samples/"+path));
+		this.audioInputStream = AudioSystem.getAudioInputStream(new File("src/fr/audioengine/samples/" + path));
 		this.clip.open(audioInputStream);
 		this.play();
 	}
@@ -82,6 +93,5 @@ public class Player {
 		this.clip.stop();
 		this.clip.close();
 	}
-
 
 }
